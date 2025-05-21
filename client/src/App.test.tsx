90dd3b152e4593b6f1a46a886e10955b08d721ec
BurgersDev <86152224+PugLord06@users.noreply.github.com>
2025-05-21 11:18:49 +0200

    Getting Ready For Publication
    - Fixed any critical issues
    - Easier compatibility
    - Removed redundant files

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import Chat from "./Frontend/chat";
+import "./styling/index.css";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
